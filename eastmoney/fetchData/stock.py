50d48ec585f71a77d295946273d00416376ba9d4
chiyuze <18359628931@qq.com>
2022-03-12 18:08:57 +0800

    Refactoring optimized code

--- eastmoney/fetchData/stock.py
+++ eastmoney/fetchData/stock.py
@@ -2,7 +2,9 @@
 import pandas as pd
 import numpy as np
 from pandas import *
-
+import requests
+import re
+import json
 
 import sys
 from pathlib import Path
@@ -17,25 +19,66 @@ from  eastmoney.kline import kline_test
 class eastStock():
 
     @classmethod
-    def get_shangzheng(self,code:str):
-        url = 'http://push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery112405093671278680167_1646568488454&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&ut=7eea3edcaed734bea9cbfc24409ed989&klt=101&fqt=1&secid=0.' + code + '&beg=0&end=20500000&_=1646568488541'
-        print('url=',url)
-        data = kline_test.request_data(url)
-        splits, name, klines = kline_test.convert_data_to_json(data, pre="jQuery112405093671278680167_1646568488454\(")
-        print("name====",name)
-        return name, splits, klines
-        # "2010-03-19,0.97,0.92,0.99,0.92,197373,1182393994.00,11.86,55.93,0.33,70.49"
-        # "2022-03-03,27.21,26.74,27.40,26.57,1455123,3914571824.00,3.06,-1.40,-0.38,1.69",
-        # "2022-03-04,26.45,26.00,26.86,25.82,1920698,5035058688.00,3.89,-2.77,-0.74,2.24"
+    def convert_javascript_to_json(self, data: str, pre=""):
+        print("start convert data----====")
+        regular = r'%s\((.*?)\)' % pre
+        # find_json = re.compile(r'jQuery33103422893090882453_1641114428433\((.*?)\)')
+        find_json = re.compile(regular)
+        all_match = re.findall(find_json, data)
+        json_str = all_match[0]
+        dic = json.loads(json_str)
+        name = dic["data"]["name"]
+        klines = dic["data"]["klines"]
+        return name, klines
+    #     "klines": [
+    #   "2010-03-19,0.97,0.92,0.99,0.92,197373,1182393994.00,11.86,55.93,0.33,70.49",
+    #   "2010-03-22,0.97,1.03,1.03,0.96,110104,693595698.00,7.61,11.96,0.11,39.32",]
+
+    @classmethod
+    def request_shangzheng(self,code: str):
+        url = "http://22.push2his.eastmoney.com/api/qt/stock/kline/get"
+        cb = "jQuery112404488523211257569_1646568864231"
+        params = {
+            "cb": cb,
+            "secid": "1." + code,
+            "ut": "fa5fd1943c7b386f172d6893dbfba10b",
+            "fields1": "f1,f2,f3,f4,f5,f6",
+            "fields2": "f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61",
+            "klt": "101",
+            "fqt": "0",
+            "end": "20500101",
+            "lmt": "1000000",
+            "_": "1646568864248"
+        }
+        head = {
+            "User-Agent": "Mozilla/5.0(Macintosh; Intel Mac OSX 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome / 96.0.4664.110 Safari / 537.36 Edg / 96.0.1054.62"
+        }
+        response = requests.get(url=url,params=params,headers=head).text
+        return self.convert_javascript_to_json(response,pre=cb)
+
+
     
     @classmethod
-    def get_shenzheng(self,code:str):
-        url = 'http://22.push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery112404488523211257569_1646568864231&secid=1.' + code + '&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=0&end=20500101&lmt=1000000&_=1646568864248'
-        print('url=',url)
-        data = kline_test.request_data(url)
-        splits, name, klines = kline_test.convert_data_to_json(data, pre="jQuery112404488523211257569_1646568864231\(")
-        print("name====",name)
-        return name, splits, klines
+    def request_shenzheng(self, code: str):
+        url = "http://push2his.eastmoney.com/api/qt/stock/kline/get"
+        cb = "jQuery112405093671278680167_1646568488454"
+        params = {
+            "cb": cb,
+            "fields1": "f1,f2,f3,f4,f5,f6",
+            "fields2": "f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61",
+            "ut": "7eea3edcaed734bea9cbfc24409ed989",
+            "klt": "101",
+            "fqt": "1",
+            "secid": "0." + code,
+            "beg": "0",
+            "end": "20500000",
+            "_": "1646568488541"
+        }
+        head = {
+            "User-Agent": "Mozilla/5.0(Macintosh; Intel Mac OSX 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome / 96.0.4664.110 Safari / 537.36 Edg / 96.0.1054.62"
+        }
+        response = requests.get(url=url,params=params,headers=head).text
+        return self.convert_javascript_to_json(response,pre=cb)
     
     @classmethod
     def save_data(self,name:str,klines):
@@ -46,12 +89,15 @@ class eastStock():
         df.to_csv(name)
         # print(df.head())
 
-# code = "300059"
+code = "300059"
 
 # name,_, klines = eastStock.get_shangzheng(code)
-code = "600519"
-name,_, klines = eastStock.get_shenzheng(code)
-eastStock.save_data(name=code + "_" + name + ".csv",klines=klines)
+# code = "600519"
+# name,_, klines = eastStock.get_shenzheng(code)
+# eastStock.save_data(name=code + "_" + name + ".csv",klines=klines)
+name, klines = eastStock.request_shenzheng(code=code)
+print(name)
+print(klines[-1])
 
 
 
